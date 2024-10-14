7082ea25dbf0657e479c8b7fa81f877f82c7bebb
Leo Freire <leofreirerr@gmail.com>
2024-10-14 03:19:43 -0300

    codigo mais organizado e falta arrumar função search

--- carros.py
+++ carros.py
@@ -1,6 +1,23 @@
 carros = []
+
+#parte estética
+bem_vindo = """BEM VINDO"""
+
+menu = """O que deseja fazer agr?
+[1] - Cadastrar um novo veículo 
+[2] - Buscar por algum veículo existente
+[3] - Mostrar carro específico ou todos os carros existentes
+[4] - Sair
+Insira a opção desejada: """
+
+menuimp = """Selecione como você deseja visualizar a lista:
+[1] - Mostrar todos os carros cadastrados
+[2] - Mostrar parte específica da lista
+Insira a opção desejada: """
+
+#funções requisitadas
 def cadastro():#função de cadastro, na main tava com erro pra gerar outros dicionario 
-    if len(carros)<=50:
+    if len(carros)<=49:
         marca = str(input("Insira a marca do carro: ")).upper()
         modelo =str(input("insira modelo do carro: ")).upper()
         ano = int(input("Insira o ano-modelo do veículo: "))
@@ -12,47 +29,35 @@ def cadastro():#função de cadastro, na main tava com erro pra gerar outros dic
             "autonomia": aut
         })
     else:
-        print("Limite de carros atingido!")
-        main()
+        print("Limite de veículos atingido!")
 
 def search():
-    global termo
-    termo = input("Digite por qual veículo você procura: ").upper()
+    termo = input("Digite marca, modelo, ano ou quantos km o veículo faz pro litro: ").upper()
     for carro in range(len(carros)):
         if termo == carros[carro]["marca"] or termo == carros[carro]["modelo"] or termo == str(carros[carro]["ano"]) or termo == str(carros[carro]["autonomia"]):
-            print("Marca: ",carros[carro]['marca'], "   |   ","Modelo: ", carros[carro]['modelo'], "   |   ", "Ano-modelo: ",carros[carro]['ano'], "   |   ","Autonomia(cidade): ",  carros[carro]['autonomia'], "km/l") #eu queria colocar num print(f) mas por algum caralho de motivo da erro qnd eu faço isso
-        else:
-            print("Elemento não encontrado")
-            main()
-        
-menuimp = """Selecione como você deseja visualizar a lista:
-[1] - Mostrar todos os carros cadastrados
-[2] - Mostrar parte específica da lista
-Insira a opção desejada: """
+            print(carros[carro]) #eu queria colocar num print(f) mas por algum caralho de motivo da erro qnd eu faço isso
+def imprimirtudo():
+    for carro in range(len(carros)):
+            print("Posição: ",carro+1,"   |   ","Marca: ",carros[carro]['marca'], "   |   ","Modelo: ", carros[carro]['modelo'], "   |   ", "Ano-modelo: ",carros[carro]['ano'], "   |   ","Autonomia(cidade): ",  carros[carro]['autonomia'], "km/l")
+
+def imprimirparte():
+    posI = int(input("Insira a posição inicial da vizualização: "))
+    posF = int(input("Insira a posição final da vizualização: "))
+    for carro in range(posI-1, posF):
+        print("Posição: ",carro+1,"   |   ","Marca: ",carros[carro]['marca'], "   |   ","Modelo: ", carros[carro]['modelo'], "   |   ", "Ano-modelo: ",carros[carro]['ano'], "   |   ","Autonomia(cidade): ",  carros[carro]['autonomia'], "km/l")
+    if posF >50:
+        print("Esse numero excede o tamanho da lista")
+
 def imprimir():#talvez eu mude o nome dessa função pq esse nome é ruim demais
     impop = int(input(menuimp))
     if impop == 1:
-        for carro in range(len(carros)):
-            print("Posição: ",carro+1,"   |   ","Marca: ",carros[carro]['marca'], "   |   ","Modelo: ", carros[carro]['modelo'], "   |   ", "Ano-modelo: ",carros[carro]['ano'], "   |   ","Autonomia(cidade): ",  carros[carro]['autonomia'], "km/l")
+        imprimirtudo()
     if impop == 2:
-        posI = int(input("Insira a posição inicial da vizualização: "))
-        posF = int(input("Insira a posição final da vizualização: "))
-        for carro in range(posI-1, posF):
-            print("Posição: ",carro+1,"   |   ","Marca: ",carros[carro]['marca'], "   |   ","Modelo: ", carros[carro]['modelo'], "   |   ", "Ano-modelo: ",carros[carro]['ano'], "   |   ","Autonomia(cidade): ",  carros[carro]['autonomia'], "km/l")
-        if posF >50:
-            print("Esse numero excede o tamanho da lista")
+        imprimirparte()
 
-bem_vindo = """BEM VINDO"""
-menu = """O que deseja fazer agr?
-[1] - Cadastrar um novo veículo 
-[2] - Buscar por algum veículo existente
-[3] - Mostrar carro específico ou todos os carros existentes
-[4] - Sair
-Insira a opção desejada: """
-
+#função principal do programa
 op = 0
 def main():
-    print(bem_vindo)
     op = int(input(menu))
     while op!= 4:
         if op == 1:
@@ -65,4 +70,7 @@ def main():
             print("Opção inválida!")
         op = int(input(menu))
     print("Obrigado por utilizar o sistema!")
+
+#chama tudo
+print(bem_vindo)
 main()
