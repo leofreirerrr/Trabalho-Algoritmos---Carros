b4c229b73252ac5586d4dbd353fd5b285532292b
Leo Freire <leofreirerr@gmail.com>
2024-10-13 23:39:56 -0300

    função de imprimir parte da lista finalizada

--- carros.py
+++ carros.py
@@ -1,4 +1,4 @@
-carros = []
+carros = [50]
 def cadastro():#função de cadastro, na main tava com erro pra gerar outros dicionario 
     marca = str(input("Insira a marca do carro: ")).upper()
     modelo =str(input("insira modelo do carro: ")).upper()
@@ -18,7 +18,23 @@ def search():
         if termo == carros[carro]["marca"] or termo == carros[carro]["modelo"] or termo == str(carros[carro]["ano"]) or termo == str(carros[carro]["autonomia"]):
             print("Marca: ",carros[carro]['marca'], "   |   ","Modelo: ", carros[carro]['modelo'], "   |   ", "Ano-modelo: ",carros[carro]['ano'], "   |   ","Autonomia(cidade): ",  carros[carro]['autonomia'], "km/l") #eu queria colocar num print(f) mas por algum caralho de motivo da erro qnd eu faço isso
 
-def 
+
+menuimp = """Selecione como você deseja visualizar a lista:
+[1] - Mostrar todos os carros cadastrados
+[2] - Mostrar parte específica da lista
+Insira a opção desejada: """
+def imprimir():#talvez eu mude o nome dessa função pq esse nome é ruim demais
+    impop = int(input(menuimp))
+    if impop == 1:
+        for carro in range(len(carros)):
+            print("Posição: ",carro+1,"   |   ","Marca: ",carros[carro]['marca'], "   |   ","Modelo: ", carros[carro]['modelo'], "   |   ", "Ano-modelo: ",carros[carro]['ano'], "   |   ","Autonomia(cidade): ",  carros[carro]['autonomia'], "km/l")
+    if impop == 2:
+        posI = int(input("Insira a posição inicial da vizualização: "))
+        posF = int(input("Insira a posição final da vizualização: "))
+        for carro in range(posI-1, posF):
+            print("Posição: ",carro+1,"   |   ","Marca: ",carros[carro]['marca'], "   |   ","Modelo: ", carros[carro]['modelo'], "   |   ", "Ano-modelo: ",carros[carro]['ano'], "   |   ","Autonomia(cidade): ",  carros[carro]['autonomia'], "km/l")
+        if posF >50:
+            print("Esse numero excede o tamanho da lista")
 
 bem_vindo = """BEM VINDO"""
 menu = """O que deseja fazer agr?
@@ -38,7 +54,7 @@ def main():
         elif op == 2:
             search()
         elif op == 3:
-            print(bem_vindo)
+            imprimir()
         else:
             print("Opção inválida!")
         op = int(input(menu))
